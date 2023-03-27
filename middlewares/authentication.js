3091f2811c7e821b7d6cbd48d7636fa0b5deee22
asimaries <logxtech59@gmail.com>
2023-03-27 23:57:17 +0530

    logout setup

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -4,13 +4,13 @@ function checkForAuthenticationCookie(cookieName) {
   return (req, res, next) => {
     const tokenCookieValue = req.cookies[cookieName]
     if (!tokenCookieValue) {
-      next()
+      return next()
     }
     try {
       const userPayload = validateToken(tokenCookieValue)
       req.user = userPayload;
     } catch (error) { }
-    next()
+    return next()
   }
 }
 
--- routes/user.js
+++ routes/user.js
@@ -10,7 +10,13 @@ router.get('/signup', (req, res) => {
   return res.render('signup')
 })
 
+router.get('/logout', (req, res) => {
+  return res.clearCookie('token').redirect('/')
+})
+
+
 router.post('/signup', handleSignUp)
 router.post('/signin', handleSignIn)
+
 
 module.exports = { router };
